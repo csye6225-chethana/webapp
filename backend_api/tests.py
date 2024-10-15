67e98b07ffc8205ead3123c4037a159990ce66bd
Chethana Benny <benny.c@northeastern.edu>
2024-10-15 15:55:42 -0400

    Update tests.py

--- backend_api/tests.py
+++ backend_api/tests.py
@@ -90,7 +90,7 @@ class UserAPITests(TestCase):
         credentials = base64.b64encode(b'testuser@gmail.com:testpassword').decode('utf-8')
         self.client.credentials(HTTP_AUTHORIZATION=f'Basic {credentials}')
         response = self.client.get(self.user_detail_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['email'], 'testuser@gmail.com')
 
     def test_get_user_details_unauthorized(self):
