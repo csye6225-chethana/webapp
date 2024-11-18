678a5f1a1c8763b95ce411b07e2cc9dfc10c4b48
Chethana Benny <benny.c@northeastern.edu>
2024-11-18 17:10:39 -0500

    changes A8

--- backend_api/models.py
+++ backend_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.utils.timezone import now
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -9,6 +10,7 @@ class User(models.Model):
     password = models.CharField(max_length=255) 
     account_created = models.DateTimeField(auto_now_add=True)
     account_updated = models.DateTimeField(auto_now=True)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
@@ -20,3 +22,19 @@ class Image(models.Model):
     url = models.URLField()
     upload_date = models.DateTimeField(auto_now_add=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+
+
+class SentEmail(models.Model):
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='sent_emails',
+        to_field='email',
+        null=True
+    )
+    token = models.CharField(max_length=255, unique=True, null=True)
+    expiration_time = models.DateTimeField()
+    sent_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"Email to {self.user.email} sent at {self.sent_at}"
